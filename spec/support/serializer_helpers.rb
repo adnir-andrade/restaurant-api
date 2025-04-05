31a7a94a0c7092a9c161dccdaafd992df6e90bcc
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-05 20:17:06 -0300

    feat: Add method to helper to assist controller specs

--- spec/support/serializer_helpers.rb
+++ spec/support/serializer_helpers.rb
@@ -8,4 +8,14 @@ module SerializerHelpers
       description: menu.description
     }.as_json
   end
+
+  def serialized_menu_item(menu_item)
+    {
+      id: menu_item.id,
+      name: menu_item.name,
+      description: menu_item.description,
+      price: format('%.2f', menu_item.price),
+      menu: menu_item.menu ? serialized_menu(menu_item.menu) : nil
+    }.as_json
+  end
 end
