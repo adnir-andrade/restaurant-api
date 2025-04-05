0fe50794124aa0fc8fe01645d0df64facb41ff55
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-05 20:17:32 -0300

    chore: Adapt MenuItem factory

--- spec/factories/menu_items.rb
+++ spec/factories/menu_items.rb
@@ -2,9 +2,18 @@
 
 FactoryBot.define do
   factory :menu_item do
-    name { 'MyString' }
-    description { 'MyText' }
-    price { '9.99' }
-    menu { nil }
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    price { Faker::Number.decimal(l_digits: 2) }
+
+    trait :with_menu do
+      association :menu
+    end
+
+    trait :pancake do
+      name { 'Pancake' }
+      description { 'I miss pancakes...' }
+      price { 7.77 }
+    end
   end
 end
