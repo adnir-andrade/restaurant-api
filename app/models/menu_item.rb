7518455d93f9602407559e18513afcc9276377de
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-05 18:23:02 -0300

    feat: Add validations to MenuItem and make menu optional

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class MenuItem < ApplicationRecord
-  belongs_to :menu
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  belongs_to :menu, optional: true
 end
