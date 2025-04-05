d7308141153d26ff2140e516109cbb8d85914c27
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-05 16:10:39 -0300

    chore: Add traits and Faker to Menu Factory

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :menu do
-    name { "MyString" }
-    description { "MyText" }
+    name { Faker::Food.ethnic_category }
+    description { Faker::Food.description }
+
+    trait :pizza do
+      name { 'Pizzas' }
+      description { 'You can never go wrong with a good Italian pizza with cheese and cheese and more cheese.' }
+    end
+
+    trait :sushi do
+      name { 'Sushi' }
+      description { 'Expensive but delicious. Soaking it in soy sauce is optional.' }
+    end
   end
 end
