1c08bf14f46940143b76ba2a93e82be84b313a08
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-05 23:58:17 -0300

    refactor: Update helper to reflect n:n relation

--- spec/support/serializer_helpers.rb
+++ spec/support/serializer_helpers.rb
@@ -15,7 +15,7 @@ module SerializerHelpers
       name: menu_item.name,
       description: menu_item.description,
       price: format('%.2f', menu_item.price),
-      menu: menu_item.menu ? serialized_menu(menu_item.menu) : nil
+      menus: menu_item.menus.map { |menu| serialized_menu(menu) }
     }.as_json
   end
 end
