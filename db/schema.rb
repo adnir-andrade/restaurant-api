0643d9aceb7d4c301783d8f463e34afb9bb941c8
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-05 23:39:23 -0300

    feat: Add unique constraint to MenuItem name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_06_022638) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_06_023558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_06_022638) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_menu_items_on_name", unique: true
   end
 
   create_table "menus", force: :cascade do |t|
