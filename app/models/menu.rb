602ce7d0dcd50be3986703412b82c27ac8ec7771
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-06 03:48:28 -0300

    chore: Disable RuboCop warning for uniqueness validation due to false positive

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Menu < ApplicationRecord
+  # rubocop:disable Rails/UniqueValidationWithoutIndex
   validates :name, presence: true, uniqueness: {
     scope: :restaurant_id,
     case_sensitive: false,
     message: 'Menu name must be unique within the same restaurant'
   }
+  # rubocop:enable Rails/UniqueValidationWithoutIndex
 
   belongs_to :restaurant
   has_many :menu_entries, dependent: :destroy
