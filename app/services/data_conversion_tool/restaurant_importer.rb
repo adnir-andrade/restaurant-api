10aee31878c18f235e900fcaffe5ea12d5329855
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-06 23:30:05 -0300

    feat: Improve response and make restaurant importer more reliable

--- app/services/data_conversion_tool/restaurant_importer.rb
+++ app/services/data_conversion_tool/restaurant_importer.rb
@@ -2,27 +2,49 @@
 
 module DataConversionTool
   class RestaurantImporter < BaseImporter
-    ALLOWED_KEYS = %w[name menus].freeze
+    ALLOWED_RESTAURANT_KEYS = %w[restaurants].freeze
 
     def import(parsed_json)
-      restaurants = parsed_json['restaurants']
-      return logs unless restaurants.is_a?(Array)
+      restaurants = extract_restaurants_from(parsed_json)
+      return { errors: @errors, logs: @logs } if restaurants.empty?
 
       restaurants.each { |restaurant_data| import_restaurant(data: restaurant_data) }
       summarize_logs
-      logs
+
+      {
+        created_records: @created_records,
+        skipped_records: @skipped_records,
+        errors: @errors,
+        skipped_keys: @skipped_keys,
+        logs: @logs
+      }
     end
 
     private
 
-    def import_restaurant(data:)
-      handle_unexpected_keys(data)
+    def extract_restaurants_from(data)
+      restaurant_key = data.keys.find { |key| self.class::ALLOWED_RESTAURANT_KEYS.include?(key) }
 
+      return handle_missing_root_key unless restaurant_key
+
+      data[restaurant_key]
+    end
+
+    def handle_missing_root_key
+      @errors << Logs.missing_restaurants_key_error(self.class::ALLOWED_RESTAURANT_KEYS)
+      []
+    end
+
+    def import_restaurant(data:)
       restaurant = create_restaurant(name: data['name'])
+      return unless restaurant
+
       menus = data['menus'] || []
 
       menu_importer = MenuImporter.new(
         restaurant: restaurant,
+        created_records: @created_records,
+        skipped_records: @skipped_records,
         logs: logs,
         errors: errors,
         skipped_keys: skipped_keys
@@ -31,15 +53,22 @@ module DataConversionTool
       menus.each { |menu_data| menu_importer.import(data: menu_data) }
     end
 
-    def handle_unexpected_keys(data)
-      KeyFinder.find(data, allowed_keys: ALLOWED_KEYS)
-      skipped_keys.merge(KeyFinder.unknown_keys)
-    end
-
     def create_restaurant(name:)
-      restaurant = Restaurant.create!(name: name)
-      logs << Logs.restaurant_success(name)
-      restaurant
+      restaurant = Restaurant.new(name: name)
+
+      if restaurant.save
+        @logs << Logs.restaurant_success(name)
+        @created_records[:restaurants] += 1
+        return restaurant
+      else
+        @errors << Logs.restaurant_creation_failed(name, restaurant.errors.full_messages.to_sentence)
+        @skipped_records[:restaurants] += 1
+      end
+
+      nil
+    rescue StandardError => e
+      @errors << Logs.restaurant_creation_exception(name, e.message)
+      nil
     end
   end
 end
