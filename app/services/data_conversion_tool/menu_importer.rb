e8aa78194f4c836267ec1ff4b4a00b153939bb83
Adnir Andrade <adnir_andrade@hotmail.com>
2025-04-06 23:54:35 -0300

    feat: Improve response and make Menu and Item importers more reliable

--- app/services/data_conversion_tool/menu_importer.rb
+++ app/services/data_conversion_tool/menu_importer.rb
@@ -4,8 +4,8 @@ module DataConversionTool
   class MenuImporter < BaseImporter
     ALLOWED_ITEM_KEYS = %w[menu_items items dishes pratos plats].freeze
 
-    def initialize(restaurant:, logs: nil, errors: nil, skipped_keys: nil)
-      super(logs: logs, errors: errors, skipped_keys: skipped_keys)
+    def initialize(restaurant:, created_records: nil, skipped_records: nil, logs: nil, errors: nil, skipped_keys: nil)
+      super(logs: logs, created_records: created_records, skipped_records: skipped_records, errors: errors, skipped_keys: skipped_keys)
       @restaurant = restaurant
     end
 
@@ -14,6 +14,8 @@ module DataConversionTool
       return handle_existing_menu(data['name']) if menu_exists?(data['name'])
 
       menu = create_menu(name: menu_name)
+      return unless menu
+
       items = extract_items_from(data)
       import_items(menu, items) if items
     end
@@ -29,9 +31,21 @@ module DataConversionTool
     end
 
     def create_menu(name:)
-      menu = @restaurant.menus.create!(name: name)
-      logs << Logs.menu_success(name, @restaurant.name)
-      menu
+      menu = @restaurant.menus.new(name: name)
+
+      if menu.save
+        @logs << Logs.menu_success(name, @restaurant.name)
+        @created_records[:menus] += 1
+        return menu
+      else
+        @errors << Logs.menu_creation_failed(name, @restaurant.name, menu.errors.full_messages.to_sentence)
+        @skipped_records[:menus] += 1
+      end
+
+      nil
+    rescue StandardError => e
+      @errors << Logs.menu_creation_exception(name, @restaurant.name, e.message)
+      nil
     end
 
     def extract_items_from(data)
@@ -58,6 +72,8 @@ module DataConversionTool
     def import_items(menu, items)
       item_importer = MenuItemImporter.new(
         menu: menu,
+        created_records: @created_records,
+        skipped_records: @skipped_records,
         logs: logs,
         errors: errors,
         skipped_keys: skipped_keys
--- app/services/data_conversion_tool/menu_item_importer.rb
+++ app/services/data_conversion_tool/menu_item_importer.rb
@@ -2,8 +2,8 @@
 
 module DataConversionTool
   class MenuItemImporter < BaseImporter
-    def initialize(menu:, logs: nil, errors: nil, skipped_keys: nil)
-      super(logs: logs, errors: errors, skipped_keys: skipped_keys)
+    def initialize(menu:, created_records: nil, skipped_records: nil, logs: nil, errors: nil, skipped_keys: nil)
+      super(logs: logs, created_records: created_records, skipped_records: skipped_records, errors: errors, skipped_keys: skipped_keys)
       @menu = menu
     end
 
@@ -12,23 +12,36 @@ module DataConversionTool
       price = data['price']
 
       if MenuItem.exists?(name: name)
-        handle_duplicate_item(name: name, price: price)
+        new_name = "#{name} (duplicate #{SecureRandom.hex(3)})"
+        create_item(name: new_name, price: price) do
+          @created_records[:duplicated_items] += 1
+          logs << Logs.duplicate_item_warning(name, new_name)
+        end
       else
-        create_item(name: name, price: price)
+        create_item(name: name, price: price) do
+          @created_records[:items] += 1
+          logs << Logs.menu_item_success(name)
+        end
       end
     end
 
     private
 
-    def handle_duplicate_item(name:, price:)
-      new_name = "#{name} (duplicate #{SecureRandom.hex(3)})"
-      @menu.menu_items.create!(name: new_name, price: price)
-      logs << Logs.duplicate_item_warning(name, new_name)
-    end
-
     def create_item(name:, price:)
-      @menu.menu_items.create!(name: name, price: price)
-      logs << Logs.menu_item_success(name)
+      item = @menu.menu_items.new(name: name, price: price)
+
+      if item.save
+        yield if block_given?
+        return item
+      else
+        @errors << Logs.menu_item_creation_failed(name, price, item.errors.full_messages.to_sentence)
+        @skipped_records[:items] += 1
+      end
+
+      nil
+    rescue StandardError => e
+      @errors << Logs.menu_item_creation_exception(name, price, e.message)
+      nil
     end
   end
 end
